9d93d4206005b4532619bb61ce9db5383eba5169
Maroxy <marie@kaifa.ch>
2022-04-25 17:39:17 +0200

    Add creation of webhook and checking if webhook exists

--- src/events/onmessage.ts
+++ src/events/onmessage.ts
@@ -1,8 +1,20 @@
+// Node imports and defining due to ESNext and Module settings
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Discord and other libraries
 import { Discord, Guard, On } from 'discordx';
 import type { ArgsOf } from 'discordx';
 import { NotBot } from '@discordx/utilities';
 import cld from 'cld';
 import axios from 'axios';
+import type { TextChannel } from 'discord.js';
+
+//@ts-expect-error
+let data = JSON.parse(fs.readFileSync(`${__dirname}/../data.json`));
 
 const locales: Set<String> = new Set(['EN', 'NL', 'DE', 'SV', 'FI', 'RU', 'BG', 'RO', 'IT', 'FR']);
 
@@ -11,6 +23,7 @@ export class Events {
     @On("messageCreate")
     @Guard(NotBot)
     async onMessage([message]: ArgsOf<"messageCreate">): Promise<void> {
+        if (message.channel.type == 'DM') return;
         cld.detect(message.content).then(async result => {
             if (result.languages.filter(language => language.code != 'en').length > 0) {
                 message.channel.sendTyping();
@@ -27,6 +40,14 @@ export class Events {
                         target_lang: 'EN'
                     }
                 });
+
+                if (!data.webhooks.filter(hook => hook.channel == message.channelId).length) {
+                    (message.channel as TextChannel).createWebhook(`Webhook #${message.channelId}`).then(webhook => {
+                        data.webhooks.push({"channel": message.channelId, "id": webhook.id, "token": webhook.token});
+
+                        fs.writeFileSync(`${__dirname}/../data.json`, JSON.stringify(data));
+                    })
+                }
 
                 if (result.languages.filter(language => language.code != 'en' && language.code != response.data.translations[0].detected_source_language.toLowerCase()).length > 0) {
                     message.channel.send('This language is not supported by DeepL')
