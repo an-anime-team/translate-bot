5ed9557771b13c73ee702edff2aa65392bd92c4d
Maroxy <marie@kaifa.ch>
2022-04-26 03:38:37 +0200

    Add Descriptions

--- src/commands/block.ts
+++ src/commands/block.ts
@@ -8,7 +8,7 @@ let data = JSON.parse(fs.readFileSync(`${__dirname}/../data.json`));
 @Permission(false)
 @Permission({ id: "910870211111059476", type: "ROLE", permission: true })
 export class BlockCommands {
-    @Slash("block")
+    @Slash("block", { description: "Disables a user from using Translate" })
     async block(@SlashOption("user", { type: "USER" }) user: User | GuildMember | undefined, interaction: CommandInteraction): Promise<void> {
         await interaction.deferReply();
         if (!user) return interaction.reply({ content: `Please provide a user!`, ephemeral: true });
@@ -17,7 +17,7 @@ export class BlockCommands {
         interaction.reply({ content: `${(user as User).username} has been banned from being able to use Translate`, ephemeral: true });
     }
 
-    @Slash("unblock")
+    @Slash("unblock", { description: "Allows a disabled user to use Translate " })
     async unblock(@SlashOption("user", { type: "USER" }) user: User | GuildMember | undefined, interaction: CommandInteraction): Promise<void> {
         await interaction.deferReply();
         if (!user) return interaction.reply({ content: `Please provide a user!`, ephemeral: true });
